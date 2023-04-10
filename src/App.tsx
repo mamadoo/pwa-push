437871148eb39aa7c10f0507fadaf863ef18854b
Mohammad Esfandiari <m.esfandiari@novin.dev>
2023-04-10 10:30:23 +0330

    fix: update notification permission

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 // import { getToken } from "./firebase";
 
 function App() {
@@ -17,37 +17,33 @@ function App() {
   // };
 
   const handleWebPush = () => {
-    window.Notification.requestPermission().then((result) => {
-      if (result === "granted") {
-        navigator.serviceWorker.ready.then((sw) => {
-          sw.pushManager
-            .subscribe({
-              userVisibleOnly: true,
-              // applicationServerKey is obtained from a server-side call to web-push `generateVAPIDKeys()`
-              applicationServerKey:
-                "BCMuZv_Kh8gREcIZU68affaEsDcK8QUI2vboMDL9QIuWnOpx5uXSkcGM7veWmBToVmohf6Iz0W3XpAlSvpW8DJE",
-            })
-            .then((pushSubscription) => {
-              console.log(JSON.stringify(pushSubscription));
-              // store this in a database etc. for you to be able to send messages to the user. later
+    navigator.serviceWorker.ready.then((sw) => {
+      sw.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+          // applicationServerKey is obtained from a server-side call to web-push `generateVAPIDKeys()`
+          applicationServerKey:
+            "BCMuZv_Kh8gREcIZU68affaEsDcK8QUI2vboMDL9QIuWnOpx5uXSkcGM7veWmBToVmohf6Iz0W3XpAlSvpW8DJE",
+        })
+        .then((pushSubscription) => {
+          console.log(JSON.stringify(pushSubscription));
+          // store this in a database etc. for you to be able to send messages to the user. later
 
-              // For testing, I add it to the web page for copy/paste testing purposes on the sever side, since the console will ellipsify/shorten the long endpoint URL
-              const p = document.createElement("p");
-              p.appendChild(
-                document.createTextNode(JSON.stringify(pushSubscription))
-              );
-              document.querySelector("html")?.appendChild(p);
+          // For testing, I add it to the web page for copy/paste testing purposes on the sever side, since the console will ellipsify/shorten the long endpoint URL
+          const p = document.createElement("p");
+          p.appendChild(
+            document.createTextNode(JSON.stringify(pushSubscription))
+          );
+          document.querySelector("html")?.appendChild(p);
 
-              // await fetch("/save-subscription", {
-              //   method: "post",
-              //   headers: {
-              //     "Content-Type": "application/json",
-              //   },
-              //   body: JSON.stringify(subscription),
-              // });
-            });
+          // await fetch("/save-subscription", {
+          //   method: "post",
+          //   headers: {
+          //     "Content-Type": "application/json",
+          //   },
+          //   body: JSON.stringify(subscription),
+          // });
         });
-      }
     });
   };
 
